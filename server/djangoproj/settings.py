ad3e9d78140ecc99bbc91badab7bc88ef1d94b15
gemps18 <150620155+gemps18@users.noreply.github.com>
2024-04-25 22:40:24 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','']
-CSRF_TRUSTED_ORIGINS = ['']
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
